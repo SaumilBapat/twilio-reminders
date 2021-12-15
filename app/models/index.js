b88bd6e6b8c6b3b4c7761501febc6553a146fa47
Saumil Bapat <saumil_bapat@hotmail.com>
2021-12-14 19:20:27 -0500

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -2,18 +2,29 @@ var env = process.env.NODE_ENV || "development";
 const dbConfig = require("../config/db.config.js")[env];
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+var sequelize;
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+if (dbConfig.use_env_variable) {
+  console.log('~~~Using Env Vars - process.env.DATABASE_URL: ' + process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+      operatorsAliases: "false",
+      dialect: 'mysql',
+      protocol: 'mysql'
+    });
+} else {
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+  
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  });
+}
 
 const db = {};
 
