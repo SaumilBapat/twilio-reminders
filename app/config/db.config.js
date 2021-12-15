1b8b8566657ace83bc2f9f8c5c041488078edfef
Saumil Bapat <saumil_bapat@hotmail.com>
2021-12-14 19:18:36 -0500

    Added Test DB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  development: {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "root",
@@ -9,5 +10,11 @@ module.exports = {
       min: 0,
       acquire: 30000,
       idle: 10000
-    }
-  };
+    },
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "mysql",
+    operatorsAliases: false
+  }
+};
