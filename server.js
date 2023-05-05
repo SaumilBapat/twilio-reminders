0e79c4c809e27586a2dd7b2c59582f25a128eaab
SaumilBapat <saumil_bapat@hotmail.com>
2023-05-05 03:38:18 -0700

    First Version - ChatGPT Based

--- server.js
+++ server.js
@@ -31,14 +31,21 @@ app.get("/", (req, res) => {
 });
 
 // Add React Routes
-var staticPath = path.join(__dirname, '/');
-app.use(express.static(staticPath));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 
 // Add Controller Routes
 require("./app/routes/app.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
