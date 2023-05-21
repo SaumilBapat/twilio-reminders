483950787f6119f126bcbdcc8f43254607c57e99
SaumilBapat <saumil_bapat@hotmail.com>
2023-05-21 19:47:17 -0400

    Update StudioClient.js

--- StudioClient.js
+++ StudioClient.js
@@ -4,11 +4,11 @@ dotenv.config()
 function InvokeStudio(reminder) {
   const accountSid = process.env.TWILIO_SID;
   const authToken = process.env.TWILIO_TOKEN;
-  const reminderUrl = 'https://twilio-reminders.herokuapp.com/api/word/' + reminder._id;
+  let reminderUrl = 'https://twilio-reminders.herokuapp.com/api/word/' + reminder._id;
   const client = require('twilio')(accountSid, authToken);
   client.studio.v2.flows('FW50c3f8489bec77b9b70010efa4c28ed5')
   .executions
-  .create({to: '+16475002510', from: '+18677940506', Parameters: {reminderText: reminder.reminderText, callbackUrl: reminderUrl}})
+  .create({to: '+16475002510', from: '+18677940506', Parameters: {reminderText: "hello", callbackUrl: 'https://twilio-reminders.herokuapp.com/api/word/646aa8c01a8c55f300f5bb68'}})
   .then(execution => console.log(execution.sid));
 }
 
